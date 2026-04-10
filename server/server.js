f367e94f33e871051615977edbdff7140aa58bbe
pranathisundara3 <pranthisundara3@gmail.com>
2026-04-10 23:51:37 +0530

    server changes updated for vercel

--- server/server.js
+++ server/server.js
@@ -43,9 +43,11 @@ app.use("/api/auth", userRouter);
 app.use("/api/message", messageRouter);
 //connect to database and start server
 await connectDb();
+if(process.env.NODE_ENV !== 'production'){
 const PORT = process.env.PORT || 5000;
 
   server.listen(PORT, () => console.log("Server is running on port :" + PORT));
 
-
-
+}
+//export server for vercel deployment
+export default server;
